47593d7c5e66eeaca92b8b21420619d6047893d7
Cezar Augusto <49259391+cezar-augusto-bs@users.noreply.github.com>
2021-11-28 18:20:22 -0400

    first commit

---
+++ app/src/main/java/br/com/meumenu/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.meumenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
